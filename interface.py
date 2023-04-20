4d0fce99e29fc1429a1f767c7bcab4698f28509a
Dinner <40739802+LuLikBP@users.noreply.github.com>
2023-04-20 11:58:12 +0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -7,15 +7,15 @@ mass = [1, 1, 1, 1, 1, 1, 1, 1]
 
 def interface():
    
-    def clicked_folder():      
+    def clicked_folder(): # функция выбора файла
         file = filedialog.askopenfilename()
         txt1.insert(tk.INSERT,file)
 
-    def clicked_xl():  
+    def clicked_xl(): # функция выбора файла 
         file = filedialog.askopenfilename()
         txt2.insert(tk.INSERT,file)
         
-    def clicked_final():  
+    def clicked_final(): 
         fnkomp = txt1.get()
         fnxl = txt2.get()
         comm = txt3.get()
@@ -142,13 +142,13 @@ def interface():
     font2 = font.Font(family= "Verdana", size=14, weight="bold", slant="roman") # настройка шрифта (жирный)
     font3 = font.Font(family= "Verdana", size=14, weight="normal", slant="roman") # настройка для текста окна
     font4 = font.Font(family= "Verdana", size=11, weight="bold", slant="roman") # настройка для текста под окном
-
+    
     lbl2 = Label(window, text=ru_txt[2], font = font2)  
     lbl2.grid(column=1, row=1)
 
     txt1 = Entry(window,width=40) 
     txt1.grid(column=1, row=2)    
-    btn1 = Button(window, text=ru_txt[3], command=clicked_folder, font = font1)  
+    btn1 = Button(window, text=ru_txt[3], command=clicked_folder, font = font4, bg="Silver")  
     btn1.grid(column=1, row=3) 
 
     lbl3 = Label(window, text="")  
@@ -159,7 +159,7 @@ def interface():
 
     txt2 = txt2 = Entry(window,width=40) 
     txt2.grid(column=1, row=6)  
-    btn2 = Button(window, text=ru_txt[5], command=clicked_xl, font = font1)  
+    btn2 = Button(window, text=ru_txt[5], command=clicked_xl, font = font4, bg="Silver")  
     btn2.grid(column=1, row=8) 
 
     lbl6 = Label(window, text="_"*60)  
@@ -171,7 +171,7 @@ def interface():
     lbl9 = Label(window, text=" ")  
     lbl9.grid(column=0, row=30)
     
-    btn3 = Button(window, text=ru_txt[6], command=clicked_final, font = font2)  
+    btn3 = Button(window, text=ru_txt[6], command=clicked_final, font = font2, bg="LightSlateGray")  
     btn3.grid(column=1, row=27,  ipadx=0, ipady=0, padx=0, pady=0)
 
     chk_state1 = IntVar()  
@@ -226,3 +226,4 @@ def interface():
     chk_state8.set(1)
     
     window.mainloop() #закрытие окна
+interface()
